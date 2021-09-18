0e2428973c6213c5ffed6eace8a824267e84fc63
RobertoFigueroa <fig18306@uvg.edu.gt>
2021-09-18 00:25:18 -0600

    [ADD] listeners for game actions

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -9,6 +9,7 @@ import "./index.module.scss";
 import { useRoom } from '../../hooks/useRoom';
 import { SocketEvents } from '../../settings';
 import { socket } from '../../sockets';
+//node v: 12.22.3
 
 export const Home = () => {
     const { mySocket, 
@@ -16,8 +17,10 @@ export const Home = () => {
         setRoom, 
         setPlayers, 
         setIsOwner, 
+        myHand,
         updateMyHand, 
         updateRivalsHand, 
+        rivalsHand,
         setMainCard, 
         setTurn, 
         setCurrentSuit, 
@@ -44,9 +47,9 @@ export const Home = () => {
 
         };
 
-        const handleGameStart = ({ game }) => {
+        const handleGameStarted = ({ game }) => {
             updateMyHand([ ...game.myHand]);
-            updateRivalsHand([...game.updateRivalsHand]);
+            updateRivalsHand([...game.cardCount]);
             setMainCard(game.principalHeap.pop());
             setTurn(game.currentPlayer);
             setCurrentSuit(game.currentSuit);
@@ -56,14 +59,47 @@ export const Home = () => {
             setWinner(winner);
         };
         
+        const handleGameMove = ({ game }) => {
+            updateRivalsHand([...game.cardCount]);
+            setMainCard(game.principalHeap.pop());
+            setTurn(game.currentPlayer);
+            setCurrentSuit(game.currentSuit);
+        };
+
+        const handleCardFromPile = ({ card, game }) => {
+           if (game.currentPlayer == mySocket) { //means its my turn and I get the card
+            updateMyHand([...myHand, card]);
+           }
+           else {
+               updateRivalsHand(game.cardCount); //it updates the cardCount of the rivals hand becuase it is not my turn
+           }
+        };
+
+        const handleTurnPassed = ({ currentPlayer }) => {
+            setTurn(currentPlayer);
+            // updateRivalsHand(); not needed because in cardFromPile already updates rivalHands
+        };
+
+        const handleTurnChanged = ({ currentPlayer }) => {
+            setTurn(currentPlayer);
+        };
+
+        const handleSuitChanged = ({ newSuit }) => {
+            setCurrentSuit(newSuit);
+        };
 
         mySocket.on(SocketEvents.DISCONNECT, handleDisconnect);
         mySocket.on(SocketEvents.ROOM_CREATED, handleRoomCreated);
         mySocket.on(SocketEvents.MESSAGE, handleMessage);
         mySocket.on(SocketEvents.ROOM_PLAYERS, handleGamePlayersInfo);
         mySocket.on(SocketEvents.ROOM_LEFT, handleRoomLeft);
-        mySocket.on(SocketEvents.GAME_START, handleGameStart);
+        mySocket.on(SocketEvents.GAME_STARTED, handleGameStarted);
         mySocket.on(SocketEvents.GAME_FINISHED, handleGameFinished);
+        mySocket.on(SocketEvents.GAME_MOVE, handleGameMove);
+        mySocket.on(SocketEvents.CARD_FROM_PILE, handleCardFromPile);
+        mySocket.on(SocketEvents.TURN_PASSED, handleTurnPassed);
+        mySocket.on(SocketEvents.TURN_CHANGED, handleTurnChanged);
+        mySocket.on(SocketEvents.SUIT_CHANGED, handleSuitChanged);
         
         return () => {
             mySocket.off(SocketEvents.DISCONNECT, handleDisconnect);
@@ -71,8 +107,9 @@ export const Home = () => {
             mySocket.off(SocketEvents.MESSAGE, handleMessage);
             mySocket.off(SocketEvents.ROOM_PLAYERS, handleGamePlayersInfo);
             mySocket.off(SocketEvents.ROOM_LEFT, handleRoomLeft);
-            mySocket.off(SocketEvents.GAME_START, handleGameStart);
+            mySocket.off(SocketEvents.GAME_START, handleGameStarted);
             mySocket.off(SocketEvents.GAME_FINISHED, handleGameFinished);
+            mySocket.off(SocketEvents.GAME_MOVE, handleGameMove);
 
         } 
     }, [mySocket])
